721bea62a897e1f90191bc203fc7d595427f0322
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-08-28 11:57:26 +0200

    Try setsid

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-java -jar utilities/receiver.jar 10001 > "kieker-receiver.log" &
-disown
+setsid java -jar utilities/receiver.jar 10001 > "kieker-receiver.log" &
 echo "Exiting..."
 exit
