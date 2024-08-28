5c594a777cc0d7f09bf9b0325296481e8bde3696
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2024-08-28 11:42:48 +0200

    Speed up process

--- start.sh
+++ start.sh
@@ -24,77 +24,6 @@ then
 	exit 1
 fi
 
-git checkout -- utilities/tools.descartes.teastore.dockerbase/Dockerfile
-git checkout -- utilities/tools.descartes.teastore.dockerbase/kieker-2.0.0-SNAPSHOT-aspectj.jar
-git checkout -- utilities/tools.descartes.teastore.dockerbase/start.sh
-
-git checkout -- utilities/tools.descartes.teastore.dockerbase/kieker.monitoring.properties
-
-git checkout -- utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/rest/*
-for file in $(find . -name "pom.xml"); do git checkout -- $file; done
-
-if [[ "$2" == "NO_INSTRUMENTATION" ]]
-then
-	
-	sed -i 's/-javaagent:\/kieker\/agent\/agent\.jar//g' utilities/tools.descartes.teastore.dockerbase/start.sh
-	for pomFile in interfaces/tools.descartes.teastore.entities/pom.xml  utilities/tools.descartes.teastore.registryclient/pom.xml #TODO Should be removed for all files, but currently only works here...
-	do
-		awk '{
-		    if (line_count > 0) { line_count--; next; }
-		    if ($0 ~ /<groupId>net.kieker-monitoring<\/groupId>/) {
-			line_count = 4;
-			next;
-		    }
-		    print;
-		}' $pomFile &> temp.xml
-		mv temp.xml $pomFile
-	done
-	
-	sed -i '/<module>\.\/utilities\/tools\.descartes\.teastore\.kieker\.probes<\/module>/d; /<module>\.\/utilities\/tools\.descartes\.teastore\.kieker\.rabbitmq<\/module>/d'  pom.xml
-	
-	for pomFile in services/tools.descartes.teastore.auth/pom.xml services/tools.descartes.teastore.persistence/pom.xml  services/tools.descartes.teastore.recommender/pom.xml  services/tools.descartes.teastore.registry/pom.xml  services/tools.descartes.teastore.webui/pom.xml
-	do
-		awk '{
-                    if (line_count > 0) { line_count--; next; }
-                    if ($0 ~ /<artifactId>kieker.probes<\/artifactId>/) {
-                        line_count = 4; prev="";
-                        next;
-                    } else {print prev;}
-                    prev=$0;
-                } END {print prev}' $pomFile &> temp.xml
-                mv temp.xml $pomFile
-       done
-       
-       for pomFile in services/tools.descartes.teastore.image/pom.xml # no space here
-	do
-		awk '{
-                    if (line_count > 0) { line_count--; next; }
-                    if ($0 ~ /<artifactId>kieker.probes<\/artifactId>/) {
-                        line_count = 3; prev="";
-                        next;
-                    } else {print prev;}
-                    prev=$0;
-                } END {print prev}' $pomFile &> temp.xml
-                mv temp.xml $pomFile
-       done
-       
-       sed -i '/^COPY kieker-2.0.0-SNAPSHOT-aspectj\.jar/d' utilities/tools.descartes.teastore.dockerbase/Dockerfile
-       rm utilities/tools.descartes.teastore.dockerbase/kieker-*
-       cp no-instrumentation-sources/* utilities/tools.descartes.teastore.registryclient/src/main/java/tools/descartes/teastore/registryclient/rest/
-fi
-
-if [[ "$2" == "DEACTIVATED" ]]
-then
-	git checkout -- utilities/tools.descartes.teastore.dockerbase/start.sh
-	sed -i 's/\(export JAVA_OPTS=".*\)"/\1 -Dkieker.monitoring.enabled=false"/' utilities/tools.descartes.teastore.dockerbase/start.sh
-fi
-
-if [[ "$2" == "NOLOGGING" ]]
-then
-	git checkout -- utilities/tools.descartes.teastore.dockerbase/start.sh
-	sed -i 's/\(export JAVA_OPTS=".*\)"/\1 -Dkieker.monitoring.writer=kieker.monitoring.writer.dump.DumpWriter -Dkieker.monitoring.core.controller.WriterController.RecordQueueFQN=kieker.monitoring.writer.dump.DumpQueue"/' utilities/tools.descartes.teastore.dockerbase/start.sh
-fi # 
-
 if [[ "$2" == "TCP" ]]
 then
 	nohup java -jar utilities/receiver.jar 10001 > "kieker-receiver.log" &
@@ -104,19 +33,6 @@ then
 	sed -i "s/kieker.monitoring.writer.tcp.SingleSocketTcpWriter.hostname=localhost/kieker.monitoring.writer.tcp.SingleSocketTcpWriter.hostname=$MY_IP/g" utilities/tools.descartes.teastore.dockerbase/kieker.monitoring.properties
 	
 fi
-
-
-echo "Building current version..."
-
-mvn clean package -DskipTests &> build.txt
-
-maven_exit_status=$?
-if [ $maven_exit_status -eq 1 ]; then
-    echo "Maven build did not succeed - cancelling"
-    exit 1
-fi
-
-cd tools && ./build_docker.sh >> ../build.txt && cd ..
 
 MY_IP=$1
 BASE_DIR=`pwd`
