ec96b3b626ff6b45124cee53bbcf74abc82e228c
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-01-12 15:05:45 +0100

    Only set RECURSION_DEPTH if it is set

--- startContainers.sh
+++ startContainers.sh
@@ -34,7 +34,10 @@ then
 	resetInstrumentationFiles
 	
 	RECURSION_DEPTH=$3
-	sed -i "/ENV USE_HTTPS false/a ENV RECURSION_DEPTH $RECURSION_DEPTH" utilities/tools.descartes.teastore.dockerbase/Dockerfile
+	if [ ! -z "${RECURSION_DEPTH}" ]
+	then
+		sed -i "/ENV USE_HTTPS false/a ENV RECURSION_DEPTH $RECURSION_DEPTH" utilities/tools.descartes.teastore.dockerbase/Dockerfile
+	fi
 	case "$2" in
 		"NO_INSTRUMENTATION") removeAllInstrumentation ;;
 		"DEACTIVATED") 
